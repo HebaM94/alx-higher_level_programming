2a385dff7ab05ff2a935ef63832b25d26581c890
Heba <hebamagde@gmail.com>
2024-01-29 23:26:02 +0200

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,9 +57,9 @@ class Rectangle:
 
     def __str__(self):
         """String representation  of a Rectangle object
-        
+
         Print the rectangle with the character #"""
-        if self.__width != 0 and  self.__height != 0:
+        if self.__width != 0 and self.__height != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
                     print("#", end="")
