b45947b9708a6bfb22fa44e0e345f77d705439fe
Heba <hebamagde@gmail.com>
2024-02-07 03:12:15 +0200

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,12 +6,8 @@ def class_to_json(obj):
     """Return the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
-    
+
     Args:
         obj: instance of a Class
     """
-    dict_descrption = {}
-    for attr_name, attr_value in obj.__dict__.items():
-        if isinstance(attr_value, (list, dict, str, int, bool)):
-            dict_descrption[attr_name] = attr_value
-    return (dict_descrption)
+    return (obj.__dict__)
