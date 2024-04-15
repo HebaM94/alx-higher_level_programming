1ca0af11ae4097602d89023189c7c6dd5b53fbc5
Heba <hebamagde@gmail.com>
2024-04-15 03:08:48 +0200

    Cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(State.name, City.id, City.name).order_by(City.id)
+    for city in cities.filter(State.id == City.state_id):
+        print(city[0] + ": (" + str(city[1]) + ") " + city[2])
