678fe25011c8e46c45c1578cd565a9e6d57e5afe
Heba <hebamagde@gmail.com>
2024-04-27 04:52:37 +0300

    update

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+#script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -s "$1" -X delete
