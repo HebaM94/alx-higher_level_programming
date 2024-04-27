f7b1aea820719b8eb70757b2558b2c359c2d075c
Heba <hebamagde@gmail.com>
2024-04-27 04:59:31 +0300

    update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -si "$1" | grep -i Allow | awk '{print $2}'
