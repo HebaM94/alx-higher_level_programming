ce89d72d58a482d5ceedf41333c7ea31d5c3a0fc
Heba <hebamagde@gmail.com>
2024-04-29 04:40:24 +0300

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
 
     if response.status_code == 200:
         commits = response.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+        for commit in commits[:10]:
+            print(commit.get('sha'), end=': ')
+            print(commit.get('commit').get('author').get('name'))
