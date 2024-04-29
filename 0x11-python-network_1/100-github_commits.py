52231130368b8b94294b3c9b7e9787033b5acbec
Heba <hebamagde@gmail.com>
2024-04-29 04:38:43 +0300

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,11 @@ import sys
 if __name__ == "__main__":
     repository = sys.argv[1]
     owner = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(repository, owner)
-
+    url = "https://api.github.com/repos/{owner}/{repository}/commits"
     response = requests.get(url)
-    commits = response.json()
+
+    if response.status_code == 200:
+        commits = response.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
