cb57dc5f66788b2cda8b5f09ed414878e88bdd55
Heba <hebamagde@gmail.com>
2024-05-22 03:37:07 +0300

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// a script that computes the number of tasks completed by user id.
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) console.error(error);
+  if (response && response.statusCode === 200) {
+    const toDos = JSON.parse(body);
+    const completedTasks = {};
+  
+    for (const toDo of toDos) {
+      if (toDo.completed === true) {
+      if (!completedTasks[toDo.userId]) {
+        completedTasks[toDo.userId] = 1;
+      } else {
+        completedTasks[toDo.userId]++;
+      }
+      }
+      }
+      console.log(completedTasks);
+    }
+});
