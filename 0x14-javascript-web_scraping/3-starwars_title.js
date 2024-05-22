f7bad921dfda41703877cdc03857bc091f1c9287
Heba <hebamagde@gmail.com>
2024-05-22 03:12:02 +0300

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//  a script that prints the title of a Star Wars movie
+//+ where the episode number matches a given integer
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const movieId = process.argv[2];
+
+request.get(url + movieId, (error, response, body) => {
+  if (error) throw error;
+  console.log(response && JSON.parse(body).title);
+});
