8026bbf1818a301a272195326a19fa27961741aa
Heba <hebamagde@gmail.com>
2024-06-02 05:49:46 +0300

    No jQuery - document loaded

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 $.ajax({
     type: 'GET',
     url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
-    success: function(data){ $('div#character').text(data.name)}
+    success: function(data){$('div#character').text(data.name)}
   });
